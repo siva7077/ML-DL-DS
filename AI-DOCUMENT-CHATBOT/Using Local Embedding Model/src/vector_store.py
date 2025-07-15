1e1eab579053d50795aacefe8fb6f6e79a978ece
siva7077 <sivacdl77@gmail.com>
2025-07-16 00:31:01 +0530

    Delete AI-DOCUMENT-CHATBOT/Using Local Embedding Model directory

--- AI-DOCUMENT-CHATBOT/Using Local Embedding Model/src/vector_store.py
+++
@@ -1,7 +0,0 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
-
-def create_vector_store(docs):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    db = FAISS.from_documents(docs, embeddings)
-    return db
